6088b3a785f5541fcc498ac49f30b445d124857a
George Betts <gbetts24@gmail.com>
2021-12-01 08:57:10 +0000

    improved day 2 by making it a single loop

--- day1/day1_part2.php
+++ day1/day1_part2.php
@@ -4,7 +4,7 @@ $values = file('input.txt', FILE_IGNORE_NEW_LINES);
 // counter for the number of times the sonar increases
 $depthIncreaseCount = 0;
 
-$measurementWindows = [];
+$comparisonValue = null;
 
 // loop each value and generate the measurment windows
 foreach ($values as $key => $value) {
@@ -13,18 +13,13 @@ foreach ($values as $key => $value) {
         break;
     }
     // generate the measurement window by suming the next 3 values
-    $measurementWindows[] = intval($value) + intval($values[$key + 1]) + intval($values[$key + 2]);
-}
-
-// loop each measurement window value and check if it increases over the previous value
-foreach ($measurementWindows as $key => $value) {
-    if ($key == 0) {
-        continue;
-    }
-    // check the value is greater than the previous value
-    if ($measurementWindows[$key - 1] < $value) {
+    $currentWindow = intval($value) + intval($values[$key + 1]) + intval($values[$key + 2]);
+    // compare the current window with the previous one to check if there is an increase
+    if ($comparisonValue != null && $currentWindow > $comparisonValue) {
         $depthIncreaseCount++;
     }
+    $comparisonValue = intval($value) + intval($values[$key + 1]) + intval($values[$key + 2]);
 }
+
 
 echo $depthIncreaseCount;
